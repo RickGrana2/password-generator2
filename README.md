c2cad739d7a7e53c768978ffd66d2954c65ab3ba
Rick Granados <Ricardo.Grana2@gmail.com>
2020-10-17 17:26:05 -0700

    template update

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # password-generator2
+// Assignment Code (L1)
+
+// Write password to the #password input(L4)
+
+// Add event listener to generate button(L13)
+
+// Array of numberic characters to be included in password
+
+// Array of lowercase characters to be included in password
+// var lowerCasedCharacters = numbericCharacter
--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,10 +9,61 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 var lowerCase=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var upperCase=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var specialChar=["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", ";", ":", ",", ".", "/", "?", "<", ">"]
 var numbers=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+
+// var passLength = 10
+// var useUpper = true
+// var useLower = true
+// var useNumbers = true
+// var useSpec = true
+
+//     function randomIndex(arr) {
+//       return arr[Math.floor(Math.random() * arr.
+//         length)]
+//     }
+
+//     function generatePassword() {
+//         var selectedOption = []
+//         var generatePassword = []
+//         var guaranteed = []
+
+//         if (useUpper) {
+//            selectedOption = selectedOption.concat
+//            (upperCasedCharacters)
+//            guaranteed.push(randomIndex
+//            (upperCasedCharacters))
+//         }
+//         if (useLower) {
+//            selectedOption = selectedOption.concat
+//            (lowerCasedCharacters)
+//            guaranteed.push(randomIndex
+//            (lowerCasedCharacters))
+//         }
+//         if (useUpper) {
+//            selectedOption = selectedOption.concat
+//            (upperCasedCharacters)
+//            guaranteed.push(randomIndex
+//            (upperCasedCharacters))
+//         }
+//         if (useUpper) {
+//            selectedOption = selectedOption.concat
+//            (upperCasedCharacters)
+//            guaranteed.push(randomIndex
+//            (upperCasedCharacters))
+//         }
+
+//         console.log(selectedOption)
+//         console.log(guaranteed)
+
+//         for(var i = 0; i < passLength; i ++){
+//           var indexToReplace
+//         }
+//         return generatedPassword() 
+//     }
